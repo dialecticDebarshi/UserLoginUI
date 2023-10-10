7406663bd2c28a9815f4b0a15cfc84abc369d4c1
dialecticDebarshi <140203504+dialecticDebarshi@users.noreply.github.com>
2023-10-11 04:12:30 +0530

    Add files via upload

---
+++ UserAccountApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserAccountApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- UserAccountApi/Models/VM_UserRegistration.cs
+++ UserAccountApi/Models/VM_UserRegistration.cs
@@ -1,1 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
 
+namespace TechnoconGroupUserModule.Models
+{
+    public class VM_UserRegistration
+    {
+
+        public int BusinessUserID { get; set; }
+        public int TenantID { get; set; }
+
+        public string CompanyGroupName { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public string ClientName { get; set; }
+        [DataType(DataType.Date)]
+        public string ClientDOB { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string ClientEmailId { get; set; }
+        [MinLength(10)]
+        [MaxLength(10)]
+        public string ClientContactNo { get; set; }
+        public int ClientUniqueCodeTypeId { get; set; }
+        public string ClientUniqueCode { get; set; }
+        public string TenantProductKey { get; set; }
+    }
+    public class VM_UserNameValidation
+    {
+        public string UserName { get; set; }
+    }
+    public class VM_TenantValidation
+    {
+        public string TenantProductKey { get; set; }
+    }
+    public class VM_UserLogin
+    {
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
+    public class VM_UserLoginResponse
+    {
+        public int UserID { get; set; }
+        public int Employee_Master_Key { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public int JobRoleId { get; set; }
+        public int UserTypeId { get; set; }
+    }
+    
+}
